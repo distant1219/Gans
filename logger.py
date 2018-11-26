475a63a96c40bdcb8a0b456b9fe5239b1c2aa967
distant1219 <distant1219@gmail.com>
2018-11-26 22:11:29 +0800

    dcgan

--- logger.py
+++ logger.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 class TFLogger():
-    def __init__(selg, log_dir):
-        self.writer = tf.Summary.FileWriter(log_dir) 
+    def __init__(self, log_dir):
+        self.writer = tf.summary.FileWriter(log_dir) 
 
     def scalar_summary(self, tag, value, step):
         summary = tf.Summary(value=[tf.Summary.Value(tag=tag, sample_value=value)])
